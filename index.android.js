00950d45674c64f57318fc04bbe21b936c02d794
ThushaanthBala <dev2@Dwarahas-Mac-mini.local>
2017-05-03 19:27:20 -0400

    This is copy of tbala
    I have integrated the Google sign-in to initial code provided by Heta.

--- index.android.js
+++ index.android.js
@@ -1,13 +1,110 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 
-import Instructions from './android/app/help/instructions'
-class AwesomeProject extends Component {
- render() {
-   return (
-     <Instructions />
-   );
- }
+// import Instructions from './android/app/help/instructions'
+// class AwesomeProject extends Component {
+//  render() {
+//    return (
+//      <Instructions />
+//    );
+//  }
+import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
+import PollsList from './android/app/List/PollsList'
+
+
+class myapp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    this._setupGoogleSignin();
+  }
+
+  render() {
+    if (!this.state.user) {
+      return (
+        <View style={styles.container}>
+         <Text style={{fontSize: 36, fontWeight: 'bold', marginBottom: 20}}>Welcome to uSnack</Text>
+         <Text style={{fontSize: 16, marginBottom: 20}}> Please login using your google account </Text>
+          <GoogleSigninButton style={{width: 120, height: 44}} color={GoogleSigninButton.Color.Light} size={GoogleSigninButton.Size.Icon} onPress={() => { this._signIn(); }}/>
+        </View>
+      );
+    }
+
+    if (this.state.user) {
+      return (
+
+        <View style={styles.container}>
+          <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>Welcome {this.state.user.name}</Text>
+          <Text style={{marginBottom: 20}}>Your email is: {this.state.user.email}</Text>
+          <PollsList />
+
+          <TouchableOpacity onPress={() => {this._signOut(); }}>
+            <View style={{marginTop: 50}}>
+              <Text>Log out</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+
+  async _setupGoogleSignin() {
+    try {
+      await GoogleSignin.hasPlayServices({ autoResolve: true });
+      await GoogleSignin.configure({
+        webClientId: '730570315128-8eqrb6pb8phkbnd7qlimghab4h199ori.apps.googleusercontent.com',
+        offlineAccess: false
+      });
+
+      const user = await GoogleSignin.currentUserAsync();
+      console.log(user);
+      this.setState({user});
+    }
+    catch(err) {
+      console.log("Play services error", err.code, err.message);
+    }
+  }
+
+  _signIn() {
+    GoogleSignin.signIn()
+    .then((user) => {
+      console.log(user);
+      this.setState({user: user});
+      this.forceUpdate();
+    })
+    .catch((err) => {
+      console.log('WRONG SIGNIN', err);
+    })
+    .done();
+  }
+
+  _signOut() {
+    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+      this.setState({user: null});
+    })
+    .done();
+  }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('myapp', () => myapp);

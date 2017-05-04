695b4ecc6431d326e71809df9b6e64790a56053a
Kyle Fowler <kyle.fowler@uptake.com>
2017-05-04 14:12:57 -0500

    ios project rename

--- ios/usnackTests/usnackTests.m
+++ ios/usnackTests/usnackTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface myappTests : XCTestCase
+@interface usnackTests : XCTestCase
 
 @end
 
-@implementation myappTests
+@implementation usnackTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

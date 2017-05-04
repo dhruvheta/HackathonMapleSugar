de100dc9444de5c2047d842f06b259249af6c053
Kyle Fowler <kyle.fowler@uptake.com>
2017-05-04 11:58:50 -0500

    it now renders (#1)

--- index.android.js
+++ index.android.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text } from 'react-native';
 
-import PollsList from './android/app/List/PollsList'
+import PollsList from './components/List/PollsList'
+
 class AwesomeProject extends Component {
  render() {
    return (
-     //<Text>Hello world!</Text>
      <PollsList />
    );
  }
